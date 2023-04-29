85af15b7e949c0e3f79d67548eef9955b9fa7a7b
Florent <florent.vincerot@me.com>
2023-04-29 15:22:47 +0200

    multiple images

--- app/models/perform.rb
+++ app/models/perform.rb
@@ -1,5 +1,5 @@
 class Perform < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  has_one_attached :photo
+  has_many_attached :photos
 end
