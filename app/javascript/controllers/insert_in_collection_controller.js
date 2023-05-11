6518550f91188310d146d80875104a7984a98bec
Florent <florent.vincerot@me.com>
2023-05-11 23:07:17 +0200

    stimulus connected

--- app/javascript/controllers/insert_in_collection_controller.js
+++ app/javascript/controllers/insert_in_collection_controller.js
@@ -2,16 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-in-collection"
 export default class extends Controller {
-  static targets = "form"
+  static targets = ["radio", "bt"]
 
   connect() {
     console.log("connecté a insert-in-collection")
     console.log("ce controller englobe :", this.element)
-    console.log("my form", this.formTarget)
+    console.log("my radio", this.radioTarget)
+    console.log("my bt", this.btTarget)
+
+  }
+  check(event) {
+    //event.preventDefault()
+    console.log(event)
+    console.log("TODO: send request in AJAX")
   }
   send(event) {
     //event.preventDefault()
 
-    console.log("TODO: send request in AJAX")
+    console.log("TODO: disable button ?")
   }
 }
